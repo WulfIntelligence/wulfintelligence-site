37192f14e1c37373107167ebdd9c0cecb22c164d
WulfIntelligence <sales@wulfintelligence.com>
2025-09-17 15:57:48 -0400

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t text-center py-6 text-xs text-gray-500">
-      © {new Date().getFullYear()} Wulf Intelligence
-    </footer>
-  )
-}
